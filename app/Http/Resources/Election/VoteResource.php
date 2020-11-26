0e7deb3cb0fa536d15d85d39a898a0579a7cd8ea
mohanadatef <mohanad_1x@yahoo.com>
2020-11-26 17:19:56 +0200

    edit vote

--- app/Http/Resources/Election/VoteResource.php
+++ app/Http/Resources/Election/VoteResource.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Resources\Election;
 
+use App\Models\Election\Vote_User;
 use App\User;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 
 class VoteResource extends JsonResource
@@ -16,9 +18,20 @@ class VoteResource extends JsonResource
      */
     public function toArray($request)
     {
-
+        $vote_user_nominee=Vote_User::where('vote_id',$this->id)->where('user_id',Auth::user()->id)->count();
+        if($vote_user_nominee)
+        {
         return [
             'vote_id' => $this->id,
+            'selected_vote' => 1,
+            'title' => $this->title,
+            'count_nominee'=>count($this->vote_nominee),
+            'vote_nominee' => VoteNomineeResource::collection($this->vote_nominee),
+        ];
+        }
+        return [
+            'vote_id' => $this->id,
+            'selected_vote' => 0,
             'title' => $this->title,
             'count_nominee'=>count($this->vote_nominee),
             'vote_nominee' => VoteNomineeResource::collection($this->vote_nominee),
