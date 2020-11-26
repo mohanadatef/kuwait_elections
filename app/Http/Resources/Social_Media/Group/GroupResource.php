d6602e44cfd4616b8e95b9f8ea922015354ca671
mohanadatef <mohanad_1x@yahoo.com>
2020-11-26 17:49:54 +0200

    group

--- app/Http/Resources/Social_Media/Group/GroupResource.php
+++ app/Http/Resources/Social_Media/Group/GroupResource.php
@@ -3,6 +3,7 @@
 namespace App\Http\Resources\Social_Media\Group;
 
 use App\Models\Image;
+use App\Models\Social_Media\Group_User;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class GroupResource extends JsonResource
@@ -15,11 +16,17 @@ class GroupResource extends JsonResource
      */
     public function toArray($request)
     {
+        $group_member=0;
         $image=Image::where('category','group')->where('category_id',$this->id)->first();
+        if($request->status_auth==1)
+        {
+            $group_member=Group_User::where('group_id',$this->id)->where('user_id',$request->user_id)->count();
+        }
         if($image)
         {
         return [
             'group_id'=>$this->id,
+            'status_join'=>$group_member,
             'title'=>$this->title,
             'about'=>$this->about,
             'profile_image'=> asset('public/images/group/'.$image->image),
@@ -27,6 +34,7 @@ class GroupResource extends JsonResource
         }
         return [
             'group_id'=>$this->id,
+            'status_join'=>$group_member,
             'title'=>$this->title,
             'about'=>$this->about,
             'profile_image'=> asset('public/images/user/profile/profile_user.jpg'),
