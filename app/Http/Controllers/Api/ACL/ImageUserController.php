03b54ae126a4185686ed92ba415634cf423fadca
mohanadatef <mohanad_1x@yahoo.com>
2020-11-28 14:02:38 +0200

    image update

--- app/Http/Controllers/Api/ACL/ImageUserController.php
+++ app/Http/Controllers/Api/ACL/ImageUserController.php
@@ -33,26 +33,24 @@ class ImageUserController extends Controller
             return response(['status' => 0, 'data' => array(), 'message' => 'برجاء الاتصال بخدمه العملاء'], 400);
         }
         $image_profile = Image::where('category', 'profile')->where('category_id', $user->id)->get();
-        $post=DB::table('posts')->where('user_id',$user->id)->where('status',1)->where('group_id',0)->pluck('id','id');
+        $post = DB::table('posts')->where('user_id', $user->id)->where('status', 1)->where('group_id', 0)->pluck('id', 'id');
         $image_post = Image::where('category', 'post')->wherein('category_id', $post)->get();
         if ($request->status_auth == 1) {
 
             $this->logRepository->Create_Data('' . Auth::user()->id . '', 'عرض', 'عرض كل الصور الشخصيه لمستخدم');
         }
         if ($image_profile) {
-            $image_profile= ProfileImageResource::collection($image_profile);
-        }
-        else{
-            $image_profile=array();
+            $image_profile = ProfileImageResource::collection($image_profile);
+        } else {
+            $image_profile = array();
         }
         if ($image_post) {
-            $image_post= PostImageResource::collection($image_post);
+            $image_post = PostImageResource::collection($image_post);
+        } else {
+            $image_post = array();
         }
-        else{
-            $image_post=array();
-        }
-        return response(['status' => 1, 'data' => ['count_image_profile'=>count($image_profile),'image_profile' =>$image_profile,
-            'count_image_post'=>count($image_post),'image_post' =>$image_post]], 200);
+        return response(['status' => 1, 'data' => ['count_image_profile' => count($image_profile), 'image_profile' => $image_profile,
+            'count_image_post' => count($image_post), 'image_post' => $image_post]], 200);
     }
 
     public function update(Request $request)
@@ -70,33 +68,32 @@ class ImageUserController extends Controller
         if ($validate->fails()) {
             return response(['status' => 0, 'data' => array(), 'message' => $validate->errors()], 422);
         }
-        $user_image = Image::find($request->image_id);
-        if ($user_image) {
-            if ($user->id == Auth::user()->id && $user_image->category_id == Auth::user()->id) {
+        if ($user->id == Auth::user()->id) {
+            $user_image = Image::where('category', 'profile')->where('category_id', Auth::user()->id)->where('status', 1)->first();
+            if ($user_image) {
                 $user_image->status = 0;
                 $user_image->save();
-                $profile_image = new Image();
-                $profile_image->category_id = $user->id;
-                $profile_image->category = 'profile';
-                $profile_image->status = 1;
-                $folderPath = public_path('images/user/profile/');
-                $image_type = 'png';
-                $image_base64 = base64_decode($request->image_profile);
-                $imageName = time() . uniqid() . '.' . $image_type;
-                $file = $folderPath . $imageName;
-                if(!\File::isDirectory($folderPath)){
-                    \File::makeDirectory($folderPath, 0777, true, true);
-                }
-                file_put_contents($file, $image_base64);
-                $profile_image->image = $imageName;
-                $profile_image->save();
-                $this->logRepository->Create_Data('' . $user->id . '', 'تعديل', 'تعديل صور الشخصيه لمستخدم');
-                return response(['status' => 1, 'data' => ['image' => new ProfileImageResource($profile_image)], 'message' => 'تم تغير الصوره الشخصيه'], 200);
             }
-            return response(['status' => 0, 'data' => array(), 'message' => 'لا يمكن اتمام الطلب'], 400);
-        }
-        return response(['status' => 0, 'data' => array(), 'message' => 'خطا فى ادخال رقم الصوره الشخصيه'], 400);
+            $profile_image = new Image();
+            $profile_image->category_id = $user->id;
+            $profile_image->category = 'profile';
+            $profile_image->status = 1;
+            $folderPath = public_path('images/user/profile/');
+            $image_type = 'png';
+            $image_base64 = base64_decode($request->image_profile);
+            $imageName = time() . uniqid() . '.' . $image_type;
+            $file = $folderPath . $imageName;
+            if (!\File::isDirectory($folderPath)) {
+                \File::makeDirectory($folderPath, 0777, true, true);
+            }
+            file_put_contents($file, $image_base64);
+            $profile_image->image = $imageName;
+            $profile_image->save();
+            $this->logRepository->Create_Data('' . $user->id . '', 'تعديل', 'تعديل صور الشخصيه لمستخدم');
+            return response(['status' => 1, 'data' => ['image' => new ProfileImageResource($profile_image)], 'message' => 'تم تغير الصوره الشخصيه'], 200);
 
+        }
+        return response(['status' => 0, 'data' => array(), 'message' => 'لا يمكن اتمام الطلب'], 400);
     }
 
     public function delete(Request $request)
